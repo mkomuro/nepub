bec233a2d0fdba988be9d9937bf074f36c93a8a2
mkomuro <16011714+mkomuro@users.noreply.github.com>
2025-12-29 22:43:32 +0900

    Updated version# to v1.4.0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ entry_points = {"console_scripts": ["nepub = nepub.__main__:main"]}
 
 setup(
     name="nepub",
-    version="1.4.0.2",
+    version="1.4.0.3",
     description="Small tool to convert Narou and Kakuyomu novels to vertically written EPUBs.",
     long_description=readme,
     author="mkomuro",
